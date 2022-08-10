ba2a3f8e9f436e4010fc97be35c1f1cbbf761e2b
artigon <itay.b31@gmail.com>
2022-08-10 20:47:48 +0300

    added timer

--- Assets/Scripts/gameSystem.cs
+++ Assets/Scripts/gameSystem.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameSystem : MonoBehaviour
 {
     public Animator blackPanel;
     public GameObject[] windows;
     private Vector3 spawnPoint;
+    private Vector3 playerWaitPoint = new Vector3(-39.69f, 2.52f, 4.67f);
     public GameObject player;
     private AudioSource sound;
     private GameObject windowSelected;
     public bool gotTheArt = false;
     public bool playerGotBack = false;
+    public bool gameOn = true;
+    private float Timer;
+    public Text TimerText;
     // Start is called before the first frame update
     void Start()
     {
-        blackPanel = GameObject.FindGameObjectWithTag("blackPanel").GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         sound = this.transform.GetChild(0).GetComponent<AudioSource>();
         windows = GameObject.FindGameObjectsWithTag("Window");
@@ -32,20 +36,41 @@ public class gameSystem : MonoBehaviour
 
         if (playerGotBack)
             StartCoroutine(winGame());
+        
+    }
+
+    IEnumerator timer()
+    {
+        if (gameOn)
+        {
+            Timer += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(Timer / 60F);
+            int seconds = Mathf.FloorToInt(Timer % 60F);
+            yield return new WaitForSeconds(0.01f);
+            TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+            StartCoroutine(timer());
+
+        }
     }
 
     IEnumerator spwnPlayer()
     {
         spawnPoint = windowSelected.GetComponent<windowMec>().windowSelected();
         sound.Play();
-        yield return new WaitForSeconds(1.3f);
+        yield return new WaitForSeconds(2f);
         blackPanel.SetBool("state", true);
+        StartCoroutine(timer());
         player.transform.position = spawnPoint;
 
     }
 
     IEnumerator winGame()
     {
+        gameOn = false;
+        print("win");
+        player.transform.position = playerWaitPoint;
         yield return new WaitForSeconds(1);
     }
+
+    
 }
